47bed311976b5ed4cb4bb5758de0f34c4a1f7dc0
nmreadelf <frank.q.kong@hotmail.com>
2023-05-10 02:12:08 +0800

    Feat sql lexer

--- src/sql/parser/lexer.h
+++ src/sql/parser/lexer.h
@@ -1,82 +1,238 @@
 #include "state.h"
 
-namespace toydb::sql {
+#include <cctype>
+#include <variant>
 
-        enum Token {
-            // A number literal
-            Number,
-            // A string literal
-            String,
-            // A textual identifier
-            Ident,
-            // The period symbol .
-            Period,
-            // The equals symbol =
-            Equals,
-            // The greater-than symbol >
-            GreaterThan,
-            // The less-than symbol <
-            LessThan,
-            // The addition symbol +
-            Plus,
-            // The subtraction symbol -
-            Minus,
-            // The multiplication symbol *
-            Asterisk,
-            // The division symbol /
-            Slash,
-            // THe exponentiation symbol ^
-            Caret,
-            // The modulo symbol %
-            Percent,
-            // THe factorial or not symbol !
-            Exclamation,
-            // THe query parameter market ?
-            Question,
-            // An opening parenthesis
-            OpenParen,
-            // A closing parenthesis
-            CloseParen,
-            // An expression separator ,
-            Comma,
-        };
-        typedef bool (*match_predicate_fun) (char);
-    typedef Status<Token> (*match_single_char_token_fun) (char);
-
-        class Lexer {
-        public:
-            Lexer(std::string& data): data_(data) {};
-
-            // Consumes any whitespace characters
-            void consume_whitespace();
-
-            // Grabs the next character if it matches the predicate function
-            Status<char> next_if(match_predicate_fun f);
-
-            // Grabs the next single-character token if the tokenizer function returns one
-            Status<Token> next_if_token(match_single_char_token_fun f);
-
-            // Grabs the next characters that match the predicate, as a string
-            Status<std::string> next_while(match_predicate_fun f);
-
-            // Scans the input for the next token if any, ignoring leading whitespace
-            Status<Token> scan();
-
-            // Scans the input for the next ident token, if any
-            Status<Token> scan_ident();
-
-            // Scans the input for the next number token, if any
-            Status<Token> scan_number();
-
-            // Scans the input for the next string literal, if any
-            Status<Token> scan_string();
-
-            // Scans the input for the next symbol token, if any
-            Status<Token> scan_symbol();
-
-
-        private:
-            std::string data_;
-        };
-
+struct Error {
+  std::string message;
 };
+
+template <typename T> using Result = std::variant<T, Error>;
+
+namespace toydb::sql {
+enum TokenType {
+  // A number literal
+  Number,
+  // A string literal
+  String,
+  // A textual identifier
+  Ident,
+  // The period symbol .
+  Period,
+  // The equals symbol =
+  Equals,
+  // The greater-than symbol >
+  GreaterThan,
+  // The less-than symbol <
+  LessThan,
+  // The addition symbol +
+  Plus,
+  // The subtraction symbol -
+  Minus,
+  // The multiplication symbol *
+  Asterisk,
+  // The division symbol /
+  Slash,
+  // THe exponentiation symbol ^
+  Caret,
+  // The modulo symbol %
+  Percent,
+  // THe factorial or not symbol !
+  Exclamation,
+  // THe query parameter market ?
+  Question,
+  // An opening parenthesis (
+  OpenParen,
+  // A closing parenthesis )
+  CloseParen,
+  // An expression separator ,
+  Comma,
+
+  // End of file or line
+  Eof,
+  // Illegal
+  Illegal,
+};
+
+struct Token {
+  TokenType Type;
+  std::string Literal;
+
+  Token(TokenType type) : Type(type) {}
+
+  Token(TokenType type, std::string &&value) : Type(type), Literal(value) {}
+
+  bool operator==(const Token &t) {
+    return Type == t.Type && Literal == t.Literal;
+  }
+};
+
+class Lexer {
+public:
+  Lexer(std::string &data)
+      : iter_(data.begin()), readIter_(data.begin()), end_(data.end()), ch_(0) {
+    readChar();
+  };
+
+  Result<Token> NextToken() {
+    skipWhitespace();
+    Token tok(Illegal);
+
+    switch (ch_) {
+    case '.':
+      tok = {Period};
+      break;
+    case '=':
+      tok = {Equals};
+      break;
+    case '>':
+      tok = {GreaterThan};
+      break;
+    case '<':
+      tok = {LessThan};
+      break;
+    case '+':
+      tok = {Plus};
+      break;
+    case '-':
+      tok = {Minus};
+      break;
+    case '*':
+      tok = {Asterisk};
+      break;
+    case '/':
+      tok = {Slash};
+      break;
+    case '^':
+      tok = {Caret};
+      break;
+    case '%':
+      tok = {Percent};
+      break;
+    case '!':
+      tok = {Exclamation};
+      break;
+    case '?':
+      tok = {Question};
+      break;
+    case '(':
+      tok = {OpenParen};
+      break;
+    case ')':
+      tok = {CloseParen};
+      break;
+    case ',':
+      tok = {Comma};
+      break;
+    case '\'':
+      return Token{String, readString()};
+      break;
+    case 0:
+      tok = {Eof};
+      break;
+    default:
+      if (std::isdigit(ch_)) {
+        return Token{Number, readNumber()};
+      } else if (std::isalpha(ch_)) {
+        return Token(Ident, readIdentifier());
+      } else {
+        tok = {Illegal};
+      }
+    }
+    readChar();
+    return tok;
+  }
+
+private:
+  void readChar() {
+    if (readIter_ >= end_) {
+      ch_ = 0;
+    } else {
+      ch_ = *readIter_;
+    }
+    iter_ = readIter_;
+    readIter_++;
+  }
+
+  std::string readIdentifier() {
+    auto pos = iter_;
+    while (std::isalnum(ch_) || ch_ == '_') {
+      readChar();
+    }
+
+    return {pos, iter_};
+  }
+
+  void skipWhitespace() {
+    while (ch_ == ' ' || ch_ == '\t' || ch_ == '\n' || ch_ == '\r') {
+      readChar();
+    }
+  }
+
+  std::string readNumber() {
+    auto pos = iter_;
+    while (std::isdigit(ch_)) {
+      readChar();
+    }
+
+    if (ch_ == '.') {
+      readChar();
+      while (std::isdigit(ch_)) {
+        readChar();
+      }
+    }
+
+    if (ch_ == 'e' || ch_ == 'E') {
+      readChar();
+      if (ch_ == '+' || ch_ == '-') {
+        readChar();
+      }
+      while (std::isdigit(ch_)) {
+        readChar();
+      }
+    }
+
+    return {pos, iter_};
+  }
+
+  // TODO: replace multi readChar, use need_if rewrite this
+  std::string readString() {
+    if (ch_ != '\'') {
+      return "";
+    }
+    readChar();
+    auto pos = iter_;
+    while (true) {
+      readChar();
+      if (ch_ == 0) {
+        break;
+      }
+      if (ch_ == '\'') {
+        if (peekChar() == '\'') {
+          readChar();
+        } else {
+          break;
+        }
+      }
+    }
+
+    std::string res(pos, iter_);
+    readChar();
+
+    return res;
+  }
+
+  char peekChar() {
+    if (readIter_ >= end_) {
+      return 0;
+    }
+
+    return *readIter_;
+  }
+
+  std::string::const_iterator iter_;
+  std::string::const_iterator readIter_;
+  std::string::const_iterator end_;
+  char ch_;
+};
+}; // namespace toydb::sql
--- src/sql/parser/lexer_test.cc
+++ src/sql/parser/lexer_test.cc
@@ -10,24 +10,122 @@ using ::testing::Test;
 
 namespace toydb::sql {
 
-    TEST(SqlLexerTest, LiteralString) {
-        std::string input(R"(A 'literal string with ''single'' and "double" quotes inside 😀'.)");
-    }
+TEST(SqlLexerTest, LiteralString) {
+  std::string input(
+      R"(A 'literal string with ''single'' and "double" quotes inside 😀'.)");
+  std::vector<Token> v{
+      Token{Ident, "A"},
+      Token{String,
+            R"(literal string with ''single'' and "double" quotes inside 😀)"},
+      Token{Period},
+  };
 
-    TEST(SqlLexerTest, Literal_number) {
-        std::string input("0 1 3.14 293. -2.718 3.14e3 2.718E-2");
-    }
+  Lexer c(input);
 
-    TEST(SqlLexerTest, Select) {
-        std::string input(R"(SELECT artist.name, album.name, EXTRACT(YEAR FROM NOW()) - album.release_year AS age
+  for (const auto &i : v) {
+    auto res = c.NextToken();
+    ASSERT_TRUE(std::get<0>(res) == i);
+  }
+}
+
+TEST(SqlLexerTest, Literal_number) {
+  std::string input("0 1 3.14 293. -2.718 3.14e3 2.718E-2");
+  std::vector<Token> v{
+      Token{Number, "0"},
+      Token{Number, "1"},
+      Token{Number, "3.14"},
+      Token{Number, "293."},
+      Token{Minus},
+      Token{Number, "2.718"},
+      Token{Number, "3.14e3"},
+      Token{Number, "2.718E-2"},
+  };
+  Lexer c(input);
+
+  for (const auto &i : v) {
+    auto res = c.NextToken();
+    ASSERT_TRUE(std::get<0>(res) == i);
+  }
+}
+
+TEST(SqlLexerTest, Select) {
+  std::string input(
+      R"(SELECT artist.name, album.name, EXTRACT(YEAR FROM NOW()) - album.release_year AS age
         FROM artist INNER JOIN album ON album.artist_id = artist.id
         WHERE album.genre != 'country' AND album.release_year >= 1980
         ORDER BY artist.name ASC, age DESC)");
-    }
+  std::vector<Token> v{
+      Token{Ident, "SELECT"},
+      Token{Ident, "artist"},
+      Token{Period},
+      Token{Ident, "name"},
+      Token{Comma},
+      Token{Ident, "album"},
+      Token{Period},
+      Token{Ident, "name"},
+      Token{Comma},
+      Token{Ident, "EXTRACT"},
+      Token{OpenParen},
+      Token{Ident, "YEAR"},
+      Token{Ident, "FROM"},
+      Token{Ident, "NOW"},
+      Token{OpenParen},
+      Token{CloseParen},
+      Token{CloseParen},
+      Token{Minus},
+      Token{Ident, "album"},
+      Token{Period},
+      Token{Ident, "release_year"},
+      Token{Ident, "AS"},
+      Token{Ident, "age"},
+      Token{Ident, "FROM"},
+      Token{Ident, "artist"},
+      Token{Ident, "INNER"},
+      Token{Ident, "JOIN"},
+      Token{Ident, "album"},
+      Token{Ident, "ON"},
+      Token{Ident, "album"},
+      Token{Period},
+      Token{Ident, "artist_id"},
+      Token{Equals},
+      Token{Ident, "artist"},
+      Token{Period},
+      Token{Ident, "id"},
+      Token{Ident, "WHERE"},
+      Token{Ident, "album"},
+      Token{Period},
+      Token{Ident, "genre"},
+      Token{Exclamation},
+      Token{Equals},
+      Token{String, "country"},
+      Token{Ident, "AND"},
+      Token{Ident, "album"},
+      Token{Period},
+      Token{Ident, "release_year"},
+      Token{GreaterThan},
+      Token{Equals},
+      Token{Number, "1980"},
+      Token{Ident, "ORDER"},
+      Token{Ident, "BY"},
+      Token{Ident, "artist"},
+      Token{Period},
+      Token{Ident, "name"},
+      Token{Ident, "ASC"},
+      Token{Comma},
+      Token{Ident, "age"},
+      Token{Ident, "DESC"},
+  };
+  Lexer c(input);
 
+  for (const auto &i : v) {
+    auto res = c.NextToken();
+    ASSERT_TRUE(std::get<0>(res) == i);
+  }
 }
 
+} // namespace toydb::sql
+
 int main(int argc, char **argv) {
-    InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+  InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
